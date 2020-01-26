6aaabe931d67f79d190318b908d91119a10c1a8d
sepehrvafaei <54388517+sepehrvafaei@users.noreply.github.com>
2020-01-25 23:34:54 -0800

    Add files via upload

---
+++ EulerProblem4.py
@@ -0,0 +1,14 @@
+def isPalindrome(n):
+    s= str(n)
+    return s ==s[::-1]
+
+def myFunc():
+    largest=0
+    for i in range(100,1000):
+        for j in range(100,1000):
+            if isPalindrome(i*j):
+                largest=i*j
+    return largest
+
+print(myFunc())
+    
