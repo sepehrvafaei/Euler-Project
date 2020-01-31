f793b87692e6b059a5834e88899411bb554613d1
sepehrvafaei <54388517+sepehrvafaei@users.noreply.github.com>
2020-01-30 17:53:45 -0800

    Update EulerProblem4.py

--- EulerProblem4.py
+++ EulerProblem4.py
@@ -6,9 +6,8 @@ def myFunc():
     largest=0
     for i in range(100,1000):
         for j in range(100,1000):
-            if isPalindrome(i*j):
+            if isPalindrome(i*j)and i*j>largest:
                 largest=i*j
     return largest
 
 print(myFunc())
-    
