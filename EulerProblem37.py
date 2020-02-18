34af3ac0d0c69e1c63ae3123659a33c4030a520b
sepehrvafaei <54388517+sepehrvafaei@users.noreply.github.com>
2020-02-17 20:40:43 -0800

    Add files via upload

---
+++ EulerProblem37.py
@@ -0,0 +1,55 @@
+import math
+def isPrime(n):
+    if n==1:
+        return False
+    elif n<=0:
+        return False
+        
+    else:
+        temp=True
+        for i in range(2,int(math.sqrt(n))+1):
+            if n%i==0:
+                temp=False
+                break
+        return temp
+
+wanted=[]
+for a in [2,3,5,7]:
+    for b in[1,3,7,9]:
+        if (a+b)%3 !=0:
+            for c in [1,3,7,9]:
+                if (a+b+c)%3 !=0:
+                    for d in [3,7]:
+                        if (a+b+c+d)%3 !=0:
+                            if (isPrime(1000*a+100*b+10*c+d) and
+                                isPrime(100*b+10*c+d) and
+                                isPrime(10*c+d) and
+                                isPrime(d) and 
+                                isPrime(100*a+10*b+c) and
+                                isPrime(10*a+b) and
+                                isPrime(a)):
+                                wanted.append(1000*a+100*b+10*c+d)
+                                
+                                
+for a in [2,3,5,7]:
+    for b in[1,3,7,9]:
+        if (a+b)%3 !=0:
+            for c in [1,3,7,9]:
+                if (a+b+c)%3 !=0:
+                    for d in [1,3,7,9]:
+                        if (a+b+c+d)%3 !=0:
+                            for e in[3,7]:
+                                if (a+b+c+d+e)%3 !=0:
+                                    if (isPrime(10000*a+1000*b+100*c+10*d+e) and
+                                        isPrime(1000*b+100*c+10*d+e) and
+                                        isPrime(100*c+10*d+e) and
+                                        isPrime(10*d+e) and 
+                                        isPrime(e) and
+                                        isPrime(1000*a+100*b+10*c+d) and
+                                        isPrime(100*a+10*b+c) and
+                                        isPrime(10*a+b) and
+                                        isPrime(a)):
+                                        wanted.append(10000*a+1000*b+100*c+10*d+e)
+
+
+print(wanted)
